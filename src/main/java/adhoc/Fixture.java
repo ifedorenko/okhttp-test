a124aeab5a610b4d1fa3ae3f725542c9ef827560
Igor Fedorenko <igor@ifedorenko.com>
2014-09-03 23:22:41 -0400

    sequencial download test
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

---
+++ src/main/java/adhoc/Fixture.java
@@ -0,0 +1,37 @@
+package adhoc;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import com.google.common.io.LineProcessor;
+
+public class Fixture {
+  public static final String BASEURL = "https://repo.maven.apache.org/maven2/";
+
+  public static List<Collection<String>> load(String filename) throws IOException {
+    File file = new File(filename);
+    return Files.readLines(file, Charsets.UTF_8, new LineProcessor<List<Collection<String>>>() {
+      List<Collection<String>> batches = new ArrayList<>();
+
+      public boolean processLine(String line) throws IOException {
+        List<String> batch = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(line, " ");
+        while (st.hasMoreTokens()) {
+          batch.add(st.nextToken());
+        }
+        batches.add(batch);
+        return true;
+      }
+
+      public List<Collection<String>> getResult() {
+        return batches;
+      }
+    });
+  }
+}
---
+++ src/main/java/adhoc/SequentialMain.java
@@ -0,0 +1,66 @@
+package adhoc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Stopwatch;
+import com.google.common.io.ByteStreams;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+public class SequentialMain {
+
+  // maven-dependencies.lst http://repo.maven.apache.org/maven2/
+  // length 41336013 byte, time 88286 ms, rate 457.23 KiB/s
+
+  // maven-dependencies.lst https://repo.maven.apache.org/maven2/
+  // length 41336013 bytes, time 80001 ms, rate 504.58 KiB/s
+
+  public static void main(String[] args) throws IOException {
+    Stopwatch stopwatch = Stopwatch.createStarted();
+    long length = new SequentialMain().downloadAll(Fixture.load("maven-dependencies.lst"));
+    long time = stopwatch.stop().elapsed(TimeUnit.MILLISECONDS);
+    float rate = (1000f / 1024f) * ((float) length) / ((float) time);
+    System.out.format("length %d bytes, time %d ms, rate %.2f KiB/s", length, time, rate);
+  }
+
+  private long downloadAll(List<Collection<String>> batches) throws IOException {
+    final OkHttpClient client = new OkHttpClient();
+
+    long length = 0;
+    for (Collection<String> batch : batches) {
+      length += downloadSync(client, batch);
+    }
+
+    return length;
+  }
+
+  private long downloadSync(OkHttpClient client, Collection<String> batch) throws IOException {
+    long length = 0;
+    for (String resource : batch) {
+      length += downloadSync(client, Fixture.BASEURL + resource);
+      length += downloadSync(client, Fixture.BASEURL + resource + ".sha1");
+    }
+    return length;
+  }
+
+  private long downloadSync(OkHttpClient client, String url) throws IOException {
+    Request request = new Request.Builder() //
+        .url(url) //
+        .build();
+    Response response = client.newCall(request).execute();
+    if (!response.isSuccessful()) {
+      throw new IOException("HTTP " + response.code() + "/" + response.message());
+    }
+    long length;
+    try (InputStream is = response.body().byteStream()) {
+      length = ByteStreams.copy(is, ByteStreams.nullOutputStream());
+    }
+    System.out.format("%s %d\n", url, length);
+    return length;
+  }
+}
